465333ddfffd48470083750a13f7b4feac78f6e7
Tvdc1998 <tomvandecruys1@gmail.com>
2026-06-19 21:23:06 +0200

    Fix Versions.kt after merge conflict

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,13 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-<<<<<<< HEAD
     const val APP_CODE = 2
     const val APP_NAME = "1.0.1"
-=======
-    const val APP_CODE = 1
-    const val APP_NAME = "1.0.0"
->>>>>>> b69d89e43a3035044e06a8a08f11960b3b6083e8
 
     const val COMPILE_SDK = 36
     const val TARGET_SDK = 36
@@ -15,8 +10,4 @@ object Versions {
     const val BUILD_TOOLS = "36.1.0"
 
     val JAVA = JavaVersion.VERSION_21
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b69d89e43a3035044e06a8a08f11960b3b6083e8
